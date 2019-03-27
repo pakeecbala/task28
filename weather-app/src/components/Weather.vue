d42c7d33c8ddca641aa18f04b076181065f1bf76
“pakeecbala” <pakeecbala@hotmail.com>
2019-03-27 09:42:58 +0100

    weather card responsive

--- weather-app/src/components/Weather.vue
+++ weather-app/src/components/Weather.vue
@@ -8,14 +8,14 @@
         <b-card-text>Humidity: N/A</b-card-text>
       </b-card>
 
-      <b-card title="Yesterday weather in Oslo">
+      <b-card title="Current weather in Oslo">
         <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image"/>
         <b-card-text>Temperature: N/A</b-card-text>
         <b-card-text>Wind: N/A</b-card-text>
         <b-card-text>Humidity: N/A</b-card-text>
       </b-card>
 
-      <b-card title="Yesterday weather in Oslo">
+      <b-card title="Tomorrow weather in Oslo">
         <b-img src="https://picsum.photos/1024/400/?image=41" fluid alt="Responsive image"/>
         <b-card-text>Temperature: N/A</b-card-text>
         <b-card-text>Wind: N/A</b-card-text>
