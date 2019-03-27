f3843a380da093e5cbc79c4f871f3a673e36d988
“pakeecbala” <pakeecbala@hotmail.com>
2019-03-27 08:56:47 +0100

    weathercard update

--- weather-app/src/components/Weather.vue
+++ weather-app/src/components/Weather.vue
@@ -3,15 +3,15 @@
     <b-card no-body class="overflow-hidden" style="max-width: 540px;">
       <b-row no-gutters>
         <b-col md="6">
-          <b-card-img src="https://picsum.photos/400/400/?image=20" class="rounded-0"/>
-        </b-col>
-        <b-col md="6">
           <b-card-body title="OSLO">
             <b-card-text>CurrentWeather</b-card-text>
             <b-card-text>Temperature: N/A</b-card-text>
             <b-card-text>Wind: N/A</b-card-text>
             <b-card-text>Humidity: N/A</b-card-text>
           </b-card-body>
+        </b-col>
+        <b-col md="6">
+          <b-card-img src="https://picsum.photos/400/400/?image=20" class="rounded-0"/>
         </b-col>
       </b-row>
     </b-card>
